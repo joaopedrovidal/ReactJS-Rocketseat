47cc04bb5a61b8fb2c4ea9dbcf33a789fc8776e0
joaopedrovidal <ppvidalsantos@gmail.com>
2024-09-17 16:01:07 -0300

    uptade App

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,28 @@
 import { Header } from "./components/Header";
 import { Post } from "./Post";
+
+import styles from './App.module.css';
+
 import './global.css';
+import { Sidebar } from "./components/Sidebar";
 
 export function App() {
   return (
     <div>
-    <Header />
-    <Post 
-      author="João Pedro" 
-      content="Adoro programação"
-    />
-    <Post 
-      author="Gustavo"
-      content="Sou o 01 do jogo do bixo"
-    />
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post 
+          author="João Pedro" 
+          content="Adoro programação"
+          />
+          <Post 
+          author="João Pedro"
+          content="Adoro futevolei"
+          />
+        </main>
+      </div>
     </div>
   )
 }
