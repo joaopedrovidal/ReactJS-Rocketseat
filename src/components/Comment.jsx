11a0ff65b7a5aa6281cfcfd90d8177d9bcc11a62
joaopedrovidal <ppvidalsantos@gmail.com>
2024-09-18 11:43:10 -0300

    Finish components "comment"

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,20 +1,21 @@
 import { ThumbsUp, Trash } from 'phosphor-react';
 import styles from './Comment.module.css';
+import { Avatar } from './Avatar';
 
 export function Comment() {
     return (
         <div className={styles.comment}>
-            <img src="https://github.com/joaopedrovidal.png" alt="" />
+            <Avatar hasBorder={false} src="https://github.com/joaopedrovidal.png"/>
             <div className={styles.commentBox}>
                 <div className={styles.commentContent}>
                     <header>
                         <div className={styles.authorAndTime}>
                             <strong>João Pedro</strong>
-                            <time title='17 de Setembro às 17:29h' dateTime='2024-09-17 17:29:48'>Cerca 1 hora atrás</time>
+                            <time title='17 de Setembro às 17:29h' dateTime='2024-09-17 17:29:48'>Cerca de 1 hora atrás</time>
                         </div>
 
                         <button title='Deletar comentário'>
-                            <Trash size={20} />
+                            <Trash size={24} />
                         </button>
                     </header>
                     <p>Muito bom João, parabéns!</p>
